8f91c9480fc28a2f2993710ef20db65953013d49
NICOLAS BELLO LUGO <n.bello@sis.virtual.uniandes.edu.co>
2018-03-19 14:33:58 -0500

    clases

--- isis2304-tutorial-arquitectura/src/vos/Cliente.java
+++ isis2304-tutorial-arquitectura/src/vos/Cliente.java
@@ -1,6 +1,97 @@
 package vos;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Cliente 
 {
-	Integer BelloSapo;
-}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// ATRIBUTOS
+	//----------------------------------------------------------------------------------------------------------------------------------
+
+	@JsonProperty(value = "id")
+	private long id;
+	
+	@JsonProperty(value = "login")
+	private String login;
+	
+	@JsonProperty (value = "password")
+	private String password;
+	
+	@JsonProperty(value = "nombre")
+	private String nombre;
+	
+	@JsonProperty (value = "correo")
+	private String correo;
+	
+	@JsonProperty(value = "documento")
+	private String documento;
+	
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODO CONSTRUCTOR
+	//----------------------------------------------------------------------------------------------------------------------------------
+	public Cliente ( @JsonProperty(value = "id") long id, @JsonProperty(value = "login") String login, @JsonProperty (value = "password") String password, 
+			@JsonProperty(value = "nombre") String nombre, @JsonProperty (value = "correo") String correo, @JsonProperty(value = "documento") String documento)
+	{
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.documento = documento;
+	}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS DE LA CLASE
+	//----------------------------------------------------------------------------------------------------------------------------------
+
+	public long getId() {
+		return id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	
+	
+}	
+
+
--- isis2304-tutorial-arquitectura/src/vos/Servicio.java
+++ isis2304-tutorial-arquitectura/src/vos/Servicio.java
@@ -1,6 +1,78 @@
 package vos;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class Servicio 
 {
-	Integer BelloSapo;
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// ATRIBUTOS
+	//----------------------------------------------------------------------------------------------------------------------------------
+	
+	@JsonProperty(value = "id")
+	private Long id;
+	@JsonProperty(value = "nombre")	
+	private String nombre;
+	@JsonProperty(value = "descripcion")	
+	private String descripcion;
+	@JsonProperty(value = "costoAgregado")	
+	private Integer costoAgregado;
+	@JsonProperty(value = "tipo")	
+	private String tipo;
+	
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODO CONSTRUCTOR
+	//----------------------------------------------------------------------------------------------------------------------------------
+	public Servicio (Long id, String nombre, String descripcion, Integer costoAgregado, String tipo) {
+		this.id = id;
+		this.nombre = nombre; 
+		this.descripcion = descripcion;
+		this.costoAgregado = costoAgregado;
+		this.tipo = tipo;
+	}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS DE LA CLASE
+	//----------------------------------------------------------------------------------------------------------------------------------
+			
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getCostoAgregado() {
+		return costoAgregado;
+	}
+
+	public void setCostoAgregado(Integer costoAgregado) {
+		this.costoAgregado = costoAgregado;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 }
