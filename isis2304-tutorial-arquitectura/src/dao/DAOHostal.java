3debc22d99ae920e6b2fd3208bd9c915211f7c02
NICOLAS BELLO LUGO <n.bello@sis.virtual.uniandes.edu.co>
2018-03-20 21:10:34 -0500

    se completan DAO falta joins

---
+++ isis2304-tutorial-arquitectura/src/dao/DAOHostal.java
@@ -0,0 +1,131 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import vos.Hostal;
+
+public class DAOHostal
+{
+	public final static String USUARIO = "ISIS2304A471810";
+
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// ATRIBUTOS
+	//----------------------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Arraylits de recursos que se usan para la ejecucion de sentencias SQL
+	 */
+	private ArrayList<Object> recursos;
+
+	/**
+	 * Atributo que genera la conexion a la base de datos
+	 */
+	private Connection conn;
+
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS DE INICIALIZACION
+	//----------------------------------------------------------------------------------------------------------------------------------
+	/**
+	 * Metodo constructor de la clase DAOHostal <br/>
+	 */
+	public DAOHostal() 
+	{
+		recursos = new ArrayList<Object>();
+	}
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS DE COMUNICACION CON LA BASE DE DATOS
+	//----------------------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Metodo que agrega la informacion de un nuevo hostal a la base de datos a partir de un parametro que ingresa
+	 * <br>
+	 * <b> Pre:</b> la conexion a sido inicializada
+	 * </br>
+	 * @param hostal hotal que se desea ingresar 
+	 * @throws SQLException SQLExcaption Se lanza una SQLException si hay un error en la base de datos o en la sentencia SQL
+	 * @throws Exception Exception si se genera un error dentro del metodo.
+	 */
+	public void addHostal (Hostal hostal) throws SQLException, Exception
+	{
+		String sentencia = String.format("INSERT INTO %1$s.HOSTALES (ID, LOGIN, PASSWORD, NOMBRE, CUENTABANCARIA, CORREO, IDENTIFICACION, HORAINICIO, HORAFIN) "
+				+ "VALUES (%2$s, '%3$s', '%4$s', '%5$s', '%6s', '%7s', '%8s','%9s', %10s')", 
+				USUARIO,
+				hostal.getId(),hostal.getLogin(),hostal.getPassword(),hostal.getNombre(),hostal.getCuentaBancaria(),hostal.getCorreo(),hostal.getDocumento(),hostal.getHoraInicio(),hostal.getHoraFin());
+		System.out.println(sentencia);
+		PreparedStatement prepStmt = conn.prepareStatement(sentencia);
+		recursos.add(prepStmt);
+		prepStmt.executeQuery();
+	}
+	
+	/**
+	 * metodo que obtiene la informacion de un hostal que se encuentra en la base de datos que tiene el identificador dado por parametro.
+	 * <br>
+	 * <b> Pre: </b> la conexion ha sido inicializada
+	 * </br>
+	 * @param id id del hostal del cual se quiere obtener la informacion
+	 * @return un ResultSet que contiene la informacion del hostal que cumple los criterios de la sentencia SQL
+	 * @throws SQLException SQLException se lanza cuando existe un error en la conexion con la base de datos o en la sentencia SQL
+	 * @throws Exception Exception que se lanza si hay errores en el codigo.
+	 */
+	
+	public ResultSet findHostalById (Long id) throws SQLException, Exception
+	{
+		String sentencia = String.format("SELECT * FROM %1$s.HOSTALES WHERE ID = %2$d", USUARIO, id); 
+		PreparedStatement prepStmt = conn.prepareStatement(sentencia);
+		recursos.add(prepStmt);
+		ResultSet result = prepStmt.executeQuery();
+		return result;
+	}
+	
+	/**
+	 * Metodo que elimina un hostal que tenga el id que llega por parametro de la base de datos
+	 * <br>
+	 * <b>Pre:</b> la conexion debe haber sido inicializada
+	 * </br>
+	 * @param id Id del hostal que se desea eliminar
+	 * @throws SQLException SQLException que se lanza cuando hay error de conexion con la base de datos o en la sentencia SQL
+	 * @throws Exception Exception que se lanza que cuando hay errores del codigo
+	 */
+	
+	public void deleteHostal(Long id) throws SQLException, Exception
+	{
+		String sentencia = String.format("DELETE FROM %1$s.HOSTALES WHERE ID = %2$d", USUARIO, id); 
+		
+		System.out.println(sentencia);
+		PreparedStatement prepStmt = conn.prepareStatement(sentencia);
+		recursos.add(prepStmt);
+		prepStmt.executeQuery();
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------------
+		// METODOS AUXILIARES
+		//----------------------------------------------------------------------------------------------------------------------------------
+
+		/**
+		 * Metodo encargado de inicializar la conexion del DAO a la Base de Datos a partir del parametro <br/>
+		 * <b>Postcondicion: </b> el atributo conn es inicializado <br/>
+		 * @param connection la conexion generada en el TransactionManager para la comunicacion con la Base de Datos
+		 */
+		public void setConn(Connection connection){
+			this.conn = connection;
+		}
+
+		/**
+		 * Metodo que cierra todos los recursos que se encuentran en el arreglo de recursos<br/>
+		 * <b>Postcondicion: </b> Todos los recurso del arreglo de recursos han sido cerrados.
+		 */
+		public void cerrarRecursos() {
+			for(Object ob : recursos){
+				if(ob instanceof PreparedStatement)
+					try {
+						((PreparedStatement) ob).close();
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+			}
+		}
+}
--- isis2304-tutorial-arquitectura/src/vos/PersonaNormal.java
+++ isis2304-tutorial-arquitectura/src/vos/PersonaNormal.java
@@ -11,7 +11,7 @@ public class PersonaNormal extends Operador
 	@JsonProperty(value="certificado")
 	private String certificado;
 	@JsonProperty(value="tipo")
-	private String tipo;
+	private TipoPersonaNatural tipo;
 	public PersonaNormal(@JsonProperty(value="id") Long id , @JsonProperty(value="login") String login,	@JsonProperty(value="nombre") String nombre, 
 			@JsonProperty(value="correo") String correo, @JsonProperty(value="password") String password, 
 			@JsonProperty(value="cuentaBancaria")String cuentaBancaria, @JsonProperty(value="documento")String documento, @JsonProperty(value="clienteEquivalente") Cliente clienteEquivalente,
@@ -20,7 +20,30 @@ public class PersonaNormal extends Operador
 	{
 		super(id, login, nombre, correo, password, cuentaBancaria, documento, clienteEquivalente, habitaciones);
 		this.certificado = certificado;
-		this.tipo = tipo;
+		if (tipo.equalsIgnoreCase(TipoPersonaNatural.EGRESADO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.EGRESADO;
+		}
+		else if (tipo.equalsIgnoreCase(TipoPersonaNatural.VECINO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.VECINO;
+		}
+		else if (tipo.equalsIgnoreCase(TipoPersonaNatural.INVITADO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.INVITADO;
+		}
+		else if (tipo.equalsIgnoreCase(TipoPersonaNatural.EMPLEADO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.EMPLEADO;
+		}
+		if (tipo.equalsIgnoreCase(TipoPersonaNatural.ESTUDIANTE.toString()))
+		{
+			this.tipo = TipoPersonaNatural.ESTUDIANTE;
+		}
+		if (tipo.equalsIgnoreCase(TipoPersonaNatural.PROFESOR.toString()))
+		{
+			this.tipo = TipoPersonaNatural.PROFESOR;
+		}
 	}
 	public String getCertificado() {
 		return certificado;
@@ -29,9 +52,33 @@ public class PersonaNormal extends Operador
 		this.certificado = certificado;
 	}
 	public String getTipo() {
-		return tipo;
+		return tipo.toString();
 	}
 	public void setTipo(String tipo) {
-		this.tipo = tipo;
+		if (tipo.equalsIgnoreCase(TipoPersonaNatural.EGRESADO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.EGRESADO;
+		}
+		else if (tipo.equalsIgnoreCase(TipoPersonaNatural.VECINO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.VECINO;
+		}
+		else if (tipo.equalsIgnoreCase(TipoPersonaNatural.INVITADO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.INVITADO;
+		}
+		else if (tipo.equalsIgnoreCase(TipoPersonaNatural.EMPLEADO.toString()))
+		{
+			this.tipo = TipoPersonaNatural.EMPLEADO;
+		}
+		if (tipo.equalsIgnoreCase(TipoPersonaNatural.ESTUDIANTE.toString()))
+		{
+			this.tipo = TipoPersonaNatural.ESTUDIANTE;
+		}
+		if (tipo.equalsIgnoreCase(TipoPersonaNatural.PROFESOR.toString()))
+		{
+			this.tipo = TipoPersonaNatural.PROFESOR;
+		}
 	}
+
 }
